4ddb0cc29d8e69b6dbc6815975ee2c0c7a374242
saerbas <savas-erbas@live.de>
2026-02-23 12:40:09 +0100

    initialize spring boot application

---
+++ smartpantry/src/main/java/com/saverba/smartpantry/SmartpantryApplication.java
@@ -0,0 +1,13 @@
+package com.saverba.smartpantry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartpantryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartpantryApplication.class, args);
+	}
+
+}
---
+++ smartpantry/src/test/java/com/saverba/smartpantry/SmartpantryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saverba.smartpantry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartpantryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
